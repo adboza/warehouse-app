c2fe3df10d356e306840690d1a5a705773597c32
Adriana Boza <adboza@gmail.com>
2022-09-15 16:21:25 -0300

    Inserido formulário e create controller.

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,14 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :description, :cep, :address, :area)
+    w = Warehouse.new(warehouse_params)
+    w.save()
+    redirect_to root_path
+  end
 end

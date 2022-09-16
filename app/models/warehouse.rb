834b590fff69e806cd9344f918bc526b69542f7e
Adriana Boza <adboza@gmail.com>
2022-09-16 17:44:31 -0300

    Adicionados testes unitários/valid código único

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,3 +1,4 @@
 class Warehouse < ApplicationRecord
   validates :name, :code, :city, :area, :address, :description, :cep, presence: true
+  validates :code, uniqueness: true
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when name is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: '', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep:'15000-000', description:'Galpão destinado para cargas internacionais')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      it 'false when code is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Guarulhos', code: '', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep:'15000-000', description:'Galpão destinado para cargas internacionais')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      it 'false when city is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Guarulhos', code: 'GRU', city: '', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep:'15000-000', description:'Galpão destinado para cargas internacionais')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      it 'false when area is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Guarulhos', code: 'GRU', city: 'Guarulhos', area: '', address: 'Avenida do Aeroporto, 1000', cep:'15000-000', description:'Galpão destinado para cargas internacionais')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      it 'false when address is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Guarulhos', code: 'GRU', city: 'Guarulhos', area: 100_000, address: '', cep:'15000-000', description:'Galpão destinado para cargas internacionais')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      it 'false when zipcode is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Guarulhos', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep:'', description:'Galpão destinado para cargas internacionais')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      it 'false when description is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Guarulhos', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep:'15000-000', description:'')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+    end
+
+    it 'false when code is already in use' do
+      #Arrange
+      first_warehouse = Warehouse.create(name: 'Guarulhos', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep:'15000-000', description:'Galpão destinado para cargas internacionais')
+
+      second_warehouse = Warehouse.new(name: 'Guarda do Embaú', code: 'GRU', city: 'Guarda do Embaú', area: 10_000, address: 'Beira do rio, 1000', cep:'85000-000', description:'Galpão destinado para cargas pesqueiras')
+
+      # Act
+      result = second_warehouse.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+  end
+
 end

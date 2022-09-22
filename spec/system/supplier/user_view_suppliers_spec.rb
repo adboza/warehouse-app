13b0278cf6dca619d692a0b56f9055417f4a190a
Adriana Boza <adboza@gmail.com>
2022-09-22 17:15:32 -0300

    Incluidos mais testes de view Fornecedores

---
+++ spec/system/supplier/user_view_suppliers_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'Usuário vê  fornecedores' do
+  it 'a partir do menu' do
+    #Arrange
+    
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    #Assert
+    expect(current_path).to eq(suppliers_path)
+  end
+  it 'a partir do menu' do
+    #Arrange
+    Supplier.create!(
+      corporate_name: 'BOZA LTDA', brand_name: 'BOZA', registration_number: '43447223000102', city: 'Curitiba', full_address: 'Torre da Indústria, 1', email: 'vendas@boza.com.br', state: 'PR')
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    #Assert
+    expect(page).to have_content('Fornecedores')
+    expect(page).to have_content('BOZA')
+    expect(page).to have_content('Curitiba - PR')
+  end
+  it 'e não existem fornecedores cadastrados' do
+    #Arrange
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    #Assert
+    expect(page).to have_content 'Não existem fornecedores cadastrados'
+  end
+end

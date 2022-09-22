23bb1e14a0bda9911c9a9884d41fc491875b0653
Adriana Boza <adboza@gmail.com>
2022-09-22 17:43:41 -0300

    yml, Show.html.erb e testes detalhes fornecedor

--- spec/system/supplier/user_view_supplier_details_spec.rb
+++ spec/system/supplier/user_view_supplier_details_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
-describe '' do
-  it '' do
-    
+describe 'Usuário vê detalhes do fornecedor' do
+  it 'a partir da tela inicial' do
+    #Arrange
+    Supplier.create!(
+      corporate_name: 'BOZA LTDA', brand_name: 'BOZA', registration_number: '43447223000102', city: 'Curitiba', full_address: 'Torre da Indústria, 1', email: 'vendas@boza.com.br', state: 'PR')
+      #Act
+      visit root_path
+      click_on 'Fornecedores'
+      click_on 'BOZA'
+      #Assert
+      expect(page).to have_content('BOZA LTDA')
+      expect(page).to have_content('Documento: 43447223000102')
+      expect(page).to have_content('Endereço: Torre da Indústria, 1')
+      expect(page).to have_content('E-mail: vendas@boza.com.br')    
+  end
+
+  it 'e volta para a tela inicial' do
+    #Arrange
+    Supplier.create!(
+      corporate_name: 'BOZA LTDA', brand_name: 'BOZA', registration_number: '43447223000102', city: 'Curitiba', full_address: 'Torre da Indústria, 1', email: 'vendas@boza.com.br', state: 'PR')
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'BOZA'
+    click_on 'Voltar'
+    #Assert
+    expect(current_path).to eq root_path
   end
 end
--- spec/system/supplier/user_view_suppliers_spec.rb
+++ spec/system/supplier/user_view_suppliers_spec.rb
@@ -10,10 +10,12 @@ describe 'Usuário vê  fornecedores' do
     #Assert
     expect(current_path).to eq(suppliers_path)
   end
-  it 'a partir do menu' do
+  it 'e visualiza lista de fornecedores' do
     #Arrange
     Supplier.create!(
       corporate_name: 'BOZA LTDA', brand_name: 'BOZA', registration_number: '43447223000102', city: 'Curitiba', full_address: 'Torre da Indústria, 1', email: 'vendas@boza.com.br', state: 'PR')
+      Supplier.create!(
+        corporate_name: 'Carloto LTDA', brand_name: 'Carloto', registration_number: '43447213000102', city: 'Campo Largo', full_address: 'Torre da Indústria, 1', email: 'vendas@carloto.com.br', state: 'PR')
     #Act
     visit root_path
     click_on 'Fornecedores'
@@ -21,6 +23,8 @@ describe 'Usuário vê  fornecedores' do
     expect(page).to have_content('Fornecedores')
     expect(page).to have_content('BOZA')
     expect(page).to have_content('Curitiba - PR')
+    expect(page).to have_content('Carloto')
+    expect(page).to have_content('Campo Largo - PR')
   end
   it 'e não existem fornecedores cadastrados' do
     #Arrange
